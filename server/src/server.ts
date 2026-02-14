60ab7e034b0712d06c278b6bf81ca16e336a27ab
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-02-15 00:30:00 +0600

    feat: Add CORS middleware, a root '/' endpoint, and update dependencies while removing package-lock.json.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,15 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+/* Middlewares */
+app.use(cors());
+
+app.get("/", (req, res) => {
+  console.log("Response sent.");
+  res.send("Hello World!");
+});
 
 app.get("/ping", (req, res) => {
   console.log("Pong.");
