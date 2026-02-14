f8edf8fa83981f36d7a4f38e0123c50242c22242
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-02-15 01:12:50 +0600

    feat: Implement SQLite database integration and add a `/users` endpoint.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,22 @@
+/* External Dependencies */
 import express from "express";
 import cors from "cors";
 
+/* Internal Dependencies */
+import databaseConnection from "./libs/db";
+
+/* Instance */
 const app = express();
+const db = await databaseConnection();
 
 /* Middlewares */
 app.use(cors());
+app.use(express.json());
 
+/* Routes */
 app.get("/", (req, res) => {
   console.log("Response sent.");
+
   res.send("Hello World!");
 });
 
@@ -15,6 +24,20 @@ app.get("/ping", (req, res) => {
   console.log("Pong.");
 
   res.send("pong");
+});
+
+app.get("/users", async (req, res) => {
+  const users = await new Promise((resolve, reject) => {
+    db.all("SELECT * FROM users", [], (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
+  });
+
+  res.send(users);
 });
 
 export default app;
