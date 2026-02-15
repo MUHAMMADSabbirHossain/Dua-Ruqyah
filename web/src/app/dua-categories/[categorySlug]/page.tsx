898bfc9e8e37027b9de4be0953ffca0e76957c85
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-02-15 22:30:21 +0600

    feat: Add basic layout and Navbar for desktop and mobile view in ui.

---
+++ web/src/app/dua-categories/[categorySlug]/page.tsx
@@ -0,0 +1,5 @@
+const CategorySlugPage = () => {
+  return <div>CategorySlugPage</div>;
+};
+
+export default CategorySlugPage;
---
+++ web/src/app/dua-categories/layout.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+const DuaCategoriesLayout = ({ children }: { children: ReactNode }) => {
+  return children;
+};
+
+export default DuaCategoriesLayout;
