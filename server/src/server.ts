47981767abc5f6c8acbaa048a551883d0682eca1
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-03-20 22:23:04 +0600

    feat: Enhance catalog API to include duas within sub-categories.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,6 @@
 /* External Dependencies */
 import express from "express";
 import cors from "cors";
-import path from "path";
 
 /* Internal Dependencies */
 import databaseConnection from "./libs/db";
@@ -83,7 +82,7 @@ app.get("/api/duas", async (req, res) => {
   });
 });
 
-app.get("/api/sub-categories-with-category", async (req, res) => {
+app.get("/api/catalog", async (req, res) => {
   await new Promise((resolve, reject) => {
     db.all(
       `SELECT category.*,
@@ -94,7 +93,25 @@ app.get("/api/sub-categories-with-category", async (req, res) => {
                 'subcat_id', sub_category.subcat_id,
                 'subcat_name_bn', sub_category.subcat_name_bn,
                 'subcat_name_en', sub_category.subcat_name_en,
-                'no_of_dua', sub_category.no_of_dua
+                'no_of_dua', sub_category.no_of_dua,
+                'duas', (
+                    SELECT JSON_GROUP_ARRAY(
+                        JSON_OBJECT(
+                            'dua_id', dua.dua_id,
+                            'dua_name_bn', dua.dua_name_bn,
+                            'dua_name_en', dua.dua_name_en,
+                            'dua_arabic', dua.dua_arabic,
+                            'transliteration_bn', dua.transliteration_bn,
+                            'transliteration_en', dua.transliteration_en,
+                            'translation_bn', dua.translation_bn,
+                            'translation_en', dua.translation_en,
+                            'audio', dua.audio
+                        )
+                    )
+                    FROM dua
+                    WHERE dua.cat_id = category.cat_id 
+                    AND dua.subcat_id = sub_category.subcat_id
+                )
             )
         )
         FROM sub_category 
@@ -109,18 +126,13 @@ ORDER BY category.id ASC;`,
           res.status(500).send(error);
         } else {
           resolve(rows);
-          // Parse the JSON string to real objects
+
           const result = rows.map((row: any) => ({
-            id: row.id,
-            cat_id: row.cat_id,
-            cat_name_bn: row.cat_name_bn,
-            cat_name_en: row.cat_name_en,
-            no_of_subcat: row.no_of_subcat,
-            no_of_dua: row.no_of_dua,
-            cat_icon: row.cat_icon,
-            sub_category: JSON.parse(row.sub_category || "[]"),
+            ...row,
+            sub_category: JSON.parse(row?.sub_category || "[]"),
           }));
-          res.status(200).send(result);
+
+          res.send(result);
         }
       },
     );
