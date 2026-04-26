0a713dc7eb2fb1698e02cc806d7f7e8e0207b12f
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-04-26 22:42:07 +0600

    refactor: convert bottom navigation bar to responsive side sidebar for desktop layout.

--- dua-ruqyah-v1/src/app/category/[slug]/page.tsx
+++ dua-ruqyah-v1/src/app/category/[slug]/page.tsx
@@ -312,7 +312,7 @@ function CategoryPage() {
   return (
     <>
       {/* Responsive Sidebar - Desktop: left sidebar, Mobile: bottom bar */}
-      <div className="fixed bottom-0 w-full bg-[#EEF6EB] border-[#E1EBE1] border-t">
+      <div className="fixed bottom-0 w-full flex items-center justify-evenly bg-[#EEF6EB] border-[#E1EBE1] left-0 border-t lg:w-[68px] lg:top-0 lg:flex-col lg:border-r">
         {/* Dua Icon - Desktop only */}
         <div className="mt-3 mb-8 hidden lg:block">
           <div className="w-[44px] h-[44px] bg-[#417360] rounded-xl flex items-center justify-center">
@@ -320,8 +320,8 @@ function CategoryPage() {
           </div>
         </div>
 
-        {/* Navigation Items - Responsive layout for Desktop only */}
-        <div className="flex justify-evenly">
+        {/* Navigation Items */}
+        <div className="flex justify-evenly w-full md:max-w-[380px] lg:mt-[43px] lg:flex-col lg:items-center lg:justify-center lg:flex-1 lg:space-y-6">
           {/* Home Icon */}
           <button className="w-10 h-10 lg:rounded-xl flex items-center justify-center">
             {svgIcons?.homeIcon}
@@ -354,7 +354,7 @@ function CategoryPage() {
         </div>
 
         {/* Bottom Icons - Desktop only */}
-        <div className="hidden">
+        <div className="hidden lg:flex flex-col items-center space-y-4 mb-3">
           {/* Trophy Icon - Desktop only */}
           <button className="w-10 h-10 rounded-xl flex items-center justify-center">
             {svgIcons?.trophyIcon}
