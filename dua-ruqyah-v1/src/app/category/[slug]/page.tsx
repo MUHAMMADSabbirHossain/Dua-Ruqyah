f8c22ab27d198841e670509db3e7ef513eed52be
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-04-26 21:49:15 +0600

    feat: implement fixed bottom navigation bar with responsive icon layout.

--- dua-ruqyah-v1/src/app/category/[slug]/page.tsx
+++ dua-ruqyah-v1/src/app/category/[slug]/page.tsx
@@ -312,58 +312,59 @@ function CategoryPage() {
   return (
     <>
       {/* Responsive Sidebar - Desktop: left sidebar, Mobile: bottom bar */}
-
-      {/* Dua Icon - Desktop only */}
-      <div className="mt-3 mb-8 hidden lg:block">
-        <div className="w-[44px] h-[44px] bg-[#417360] rounded-xl flex items-center justify-center">
-          {svgIcons?.duaIcon}
+      <div className="fixed bottom-0 w-full bg-[#EEF6EB] border-[#E1EBE1] border-t">
+        {/* Dua Icon - Desktop only */}
+        <div className="mt-3 mb-8 hidden lg:block">
+          <div className="w-[44px] h-[44px] bg-[#417360] rounded-xl flex items-center justify-center">
+            {svgIcons?.duaIcon}
+          </div>
         </div>
-      </div>
 
-      {/* Navigation Items - Responsive layout for Desktop only */}
-      <div>
-        {/* Home Icon */}
-        <button className="w-10 h-10 lg:rounded-xl flex items-center justify-center">
-          {svgIcons?.homeIcon}
-        </button>
+        {/* Navigation Items - Responsive layout for Desktop only */}
+        <div className="flex justify-evenly">
+          {/* Home Icon */}
+          <button className="w-10 h-10 lg:rounded-xl flex items-center justify-center">
+            {svgIcons?.homeIcon}
+          </button>
 
-        {/* Bookmark Icon */}
-        <button className="w-10 h-10 lg:rounded-xl flex items-center justify-center">
-          {svgIcons?.bookmarkIcon}
-        </button>
+          {/* Bookmark Icon */}
+          <button className="w-10 h-10 lg:rounded-xl flex items-center justify-center">
+            {svgIcons?.bookmarkIcon}
+          </button>
 
-        {/* Play Icon */}
-        <button className="w-10 h-10 lg:rounded-xl flex items-center justify-center">
-          {svgIcons?.playIcon}
-        </button>
+          {/* Play Icon */}
+          <button className="w-10 h-10 lg:rounded-xl flex items-center justify-center">
+            {svgIcons?.playIcon}
+          </button>
 
-        {/* Grid/Categories Icon */}
-        <button className="w-10 h-10 lg:rounded-xl flex items-center justify-center">
-          {svgIcons?.categoryIcon}
-        </button>
+          {/* Grid/Categories Icon */}
+          <button className="w-10 h-10 lg:rounded-xl flex items-center justify-center">
+            {svgIcons?.categoryIcon}
+          </button>
 
-        {/* More/Menu Icon */}
-        <button className="w-10 h-10 lg:rounded-xl flex items-center justify-center">
-          {svgIcons?.moreIcon}
-        </button>
+          {/* More/Menu Icon */}
+          <button className="w-10 h-10 lg:rounded-xl flex items-center justify-center">
+            {svgIcons?.moreIcon}
+          </button>
 
-        {/* Book Icon */}
-        <button className="w-10 h-10 lg:rounded-xl flex items-center justify-center">
-          {svgIcons?.bookIcon}
-        </button>
-      </div>
+          {/* Book Icon */}
+          <button className="w-10 h-10 lg:rounded-xl flex items-center justify-center">
+            {svgIcons?.bookIcon}
+          </button>
+        </div>
 
-      {/* Bottom Icons - Desktop only */}
-      <div>
-        {/* Trophy Icon - Desktop only */}
-        <button className="w-10 h-10 rounded-xl flex items-center justify-center">
-          {svgIcons?.trophyIcon}
-        </button>
+        {/* Bottom Icons - Desktop only */}
+        <div className="hidden">
+          {/* Trophy Icon - Desktop only */}
+          <button className="w-10 h-10 rounded-xl flex items-center justify-center">
+            {svgIcons?.trophyIcon}
+          </button>
 
-        {/* Menu Icon - Desktop only */}
-        <button className="w-10 h-10 rounded-xl flex items-center justify-center">
-          {svgIcons?.menuIcon}
-        </button>
+          {/* Menu Icon - Desktop only */}
+          <button className="w-10 h-10 rounded-xl flex items-center justify-center">
+            {svgIcons?.menuIcon}
+          </button>
+        </div>
       </div>
     </>
   );
