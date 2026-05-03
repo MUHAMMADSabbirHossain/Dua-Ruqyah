c025a17bac144b6d82af7d038a1e5f8d400912d6
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-05-04 00:24:52 +0600

    feat: api fetch and render catalog categories in the category page sidebar.

--- dua-ruqyah-v1/src/app/category/[slug]/page.tsx
+++ dua-ruqyah-v1/src/app/category/[slug]/page.tsx
@@ -2,13 +2,39 @@
 
 import { Poppins } from "next/font/google";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const poppinsFont = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
   display: "swap",
 });
+
+function renderIcon(iconPath?: string | null) {
+  if (iconPath) {
+    return (
+      <Image
+        src={iconPath}
+        alt="Icon"
+        width={44}
+        height={44}
+        className="rounded-[15px]"
+      />
+    );
+  } else {
+    return (
+      <svg
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect width="20" height="20" rx="4" fill="#E1EBE1" />
+      </svg>
+    );
+  }
+}
 
 function CategoryPage() {
   const svgIcons = {
@@ -396,11 +422,34 @@ function CategoryPage() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
   const [arabicFontSize, setArabicFontSize] = useState<number>(24);
   const [translationFontSize, setTranslationFontSize] = useState<number>(18);
+  const [catelogCategories, setCatelogCategories] = useState([]);
 
   const handleToggleMobileMenu = (open?: boolean) => {
     setIsMobileMenuOpen((prev) => (open === undefined ? !prev : open));
   };
 
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const result = await fetch("/api/v1/catelog/category");
+        const data = await result.json();
+        // console.log(data);
+
+        if (
+          data?.status === 200 &&
+          data?.success === true &&
+          data?.data?.length > 0
+        ) {
+          setCatelogCategories(data?.data || []);
+        }
+      } catch (error) {
+        console.log("error: ", error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  // console.log(catelogCategories);
   return (
     <>
       {/* Responsive Sidebar - Desktop: left sidebar, Mobile: bottom bar */}
@@ -582,176 +631,210 @@ function CategoryPage() {
           />
         )}
 
-        {/* Right Sidebar */}
-        <div className="hidden 2xl:block">
-          <div
-            className={`bg-[#F7FFFB] h-[calc(100vh-64px)] w-[352px] pt-[14px] border-l border-[#E1EBE1]`}
-          >
-            {/* Font Settings Section */}
-            <div className="px-6">
-              <div className="flex items-center gap-4 py-[10px]">
-                <div className="ml-2">
-                  <Image
-                    src="/icons/font-settings-icon.svg"
-                    alt="Font Settings"
-                    width={34}
-                    height={34}
-                  />
-                </div>
+        <div className="flex">
+          {/* Category Catelog Section - Desktop Only*/}
+          <div className="">
+            {/* Search Bar */}
+            <div>Search Bar</div>
 
-                <h3 className="text-[#417360] text-sm font-semibold flex-1">
-                  Font Settings
-                </h3>
+            {/* Category */}
+            <div className="">
+              {catelogCategories.map((catelogCategory) => (
+                <div key={catelogCategory?.id} className="">
+                  {/* Level 1 */}
+                  <div>
+                    <div>
+                      <div>{renderIcon(catelogCategory?.icon_path)}</div>
+                      <div>
+                        <h3>{catelogCategory?.name}</h3>
 
-                <div className="mr-2">
-                  <Image
-                    src="/icons/arrow-up-icon.svg"
-                    alt="Up Arrow Icon"
-                    width={14}
-                    height={14}
-                  />
-                </div>
-              </div>
-
-              <div className="mt-6 space-y-[22px]">
-                {/* Arabic Font Size */}
-                <div className="pb-2 px-2">
-                  <div className="px-2 mb-3">
-                    <label
-                      htmlFor=""
-                      className="text-[#282E29] text-sm font-medium"
-                    >
-                      Arabic Font Size
-                    </label>
-                  </div>
-
-                  <div className="flex items-center gap-4 px-2">
-                    <div className="flex-1">
-                      <input
-                        type="range"
-                        name=""
-                        id=""
-                        value={arabicFontSize}
-                        onChange={(e) =>
-                          setArabicFontSize(Number(e.target.value))
-                        }
-                        min={12}
-                        max={40}
-                        step={1}
-                        style={{
-                          background: `linear-gradient(90deg, #417360 ${((arabicFontSize - 12) / 28) * 100}%, #e5e7eb ${((arabicFontSize - 12) / 28) * 100}%)`,
-                        }}
-                        className="w-full h-2 appearance-none cursor-pointer accent-[#417360] rounded-full"
-                      />
+                        <div>
+                          <span>
+                            {catelogCategory?.subcategories?.length}{" "}
+                            Subcategories
+                          </span>
+                        </div>
+                      </div>
                     </div>
-                    <span className="text-[#417360] text-sm font-semibold">
-                      {arabicFontSize}
-                    </span>
+
+                    {/* Expand/Collapse Button */}
                   </div>
                 </div>
-
-                {/* Translation Font Size */}
-                <div className="pb-2 px-2">
-                  <div className="px-2 mb-3">
-                    <label
-                      htmlFor=""
-                      className="text-[#282E29] text-sm font-medium"
-                    >
-                      Translation Font Size
-                    </label>
-                  </div>
-
-                  <div className="flex items-center gap-4 px-2">
-                    <div className="flex-1">
-                      <input
-                        type="range"
-                        name=""
-                        id=""
-                        value={translationFontSize}
-                        onChange={(e) =>
-                          setTranslationFontSize(Number(e.target.value))
-                        }
-                        min={12}
-                        max={40}
-                        step={1}
-                        style={{
-                          background: `linear-gradient(90deg, #417360 ${((translationFontSize - 12) / 28) * 100}%, #e5e7eb ${((translationFontSize - 12) / 28) * 100}%)`,
-                        }}
-                        className="w-full h-2 appearance-none cursor-pointer accent-[#417360] rounded-full"
-                      />
-                    </div>
-                    <span className="text-[#417360] text-sm font-semibold">
-                      {translationFontSize}
-                    </span>
-                  </div>
-                </div>
-
-                {/* Arabic Script & Font Face */}
-                <div className="pb-2">
-                  <div className="px-2 mb-3">
-                    <label
-                      htmlFor=""
-                      className="text-[#282E29] text-sm font-medium"
-                    >
-                      Translation Font Size
-                    </label>
-                  </div>
-                  <div className="mx-2">
-                    <div className="bg-[#EBF9EC] rounded-lg border border-[#E2E8F0] py-[13px] px-4 flex items-center justify-between cursor-pointer hover:bg-[#F8FAFC] transition-colors duration-300 ease-linear">
-                      <span className="text-[#282E29] text-sm">Uthma</span>
-                      {svgIcons.rightArrowIcon}
-                    </div>
-                  </div>
-                </div>
-              </div>
+              ))}
             </div>
+          </div>
 
-            {/* View Settings Section */}
-            <div className="px-6 pb-6">
-              <div className="flex items-center gap-4 cursor-pointer py-[10px] rounded-xl">
-                <div className="ml-2">
-                  <Image
-                    src="/icons/view-settings-icon.svg"
-                    alt="View Settings"
-                    width={34}
-                    height={34}
-                  />
+          {/* Right Sidebar */}
+          <div className="hidden 2xl:block">
+            <div
+              className={`bg-[#F7FFFB] h-[calc(100vh-64px)] w-[352px] pt-[14px] border-l border-[#E1EBE1]`}
+            >
+              {/* Font Settings Section */}
+              <div className="px-6">
+                <div className="flex items-center gap-4 py-[10px]">
+                  <div className="ml-2">
+                    <Image
+                      src="/icons/font-settings-icon.svg"
+                      alt="Font Settings"
+                      width={34}
+                      height={34}
+                    />
+                  </div>
+
+                  <h3 className="text-[#417360] text-sm font-semibold flex-1">
+                    Font Settings
+                  </h3>
+
+                  <div className="mr-2">
+                    <Image
+                      src="/icons/arrow-up-icon.svg"
+                      alt="Up Arrow Icon"
+                      width={14}
+                      height={14}
+                    />
+                  </div>
                 </div>
-                <h3 className="text-[#282E29] text-sm font-medium flex-1">
-                  View Settings
-                </h3>
-                <div className="mr-2">
-                  <Image
-                    src="/icons/arrow-down.svg"
-                    alt="Arrow"
-                    width={14}
-                    height={14}
-                  />
+
+                <div className="mt-6 space-y-[22px]">
+                  {/* Arabic Font Size */}
+                  <div className="pb-2 px-2">
+                    <div className="px-2 mb-3">
+                      <label
+                        htmlFor=""
+                        className="text-[#282E29] text-sm font-medium"
+                      >
+                        Arabic Font Size
+                      </label>
+                    </div>
+
+                    <div className="flex items-center gap-4 px-2">
+                      <div className="flex-1">
+                        <input
+                          type="range"
+                          name=""
+                          id=""
+                          value={arabicFontSize}
+                          onChange={(e) =>
+                            setArabicFontSize(Number(e.target.value))
+                          }
+                          min={12}
+                          max={40}
+                          step={1}
+                          style={{
+                            background: `linear-gradient(90deg, #417360 ${((arabicFontSize - 12) / 28) * 100}%, #e5e7eb ${((arabicFontSize - 12) / 28) * 100}%)`,
+                          }}
+                          className="w-full h-2 appearance-none cursor-pointer accent-[#417360] rounded-full"
+                        />
+                      </div>
+                      <span className="text-[#417360] text-sm font-semibold">
+                        {arabicFontSize}
+                      </span>
+                    </div>
+                  </div>
+
+                  {/* Translation Font Size */}
+                  <div className="pb-2 px-2">
+                    <div className="px-2 mb-3">
+                      <label
+                        htmlFor=""
+                        className="text-[#282E29] text-sm font-medium"
+                      >
+                        Translation Font Size
+                      </label>
+                    </div>
+
+                    <div className="flex items-center gap-4 px-2">
+                      <div className="flex-1">
+                        <input
+                          type="range"
+                          name=""
+                          id=""
+                          value={translationFontSize}
+                          onChange={(e) =>
+                            setTranslationFontSize(Number(e.target.value))
+                          }
+                          min={12}
+                          max={40}
+                          step={1}
+                          style={{
+                            background: `linear-gradient(90deg, #417360 ${((translationFontSize - 12) / 28) * 100}%, #e5e7eb ${((translationFontSize - 12) / 28) * 100}%)`,
+                          }}
+                          className="w-full h-2 appearance-none cursor-pointer accent-[#417360] rounded-full"
+                        />
+                      </div>
+                      <span className="text-[#417360] text-sm font-semibold">
+                        {translationFontSize}
+                      </span>
+                    </div>
+                  </div>
+
+                  {/* Arabic Script & Font Face */}
+                  <div className="pb-2">
+                    <div className="px-2 mb-3">
+                      <label
+                        htmlFor=""
+                        className="text-[#282E29] text-sm font-medium"
+                      >
+                        Translation Font Size
+                      </label>
+                    </div>
+                    <div className="mx-2">
+                      <div className="bg-[#EBF9EC] rounded-lg border border-[#E2E8F0] py-[13px] px-4 flex items-center justify-between cursor-pointer hover:bg-[#F8FAFC] transition-colors duration-300 ease-linear">
+                        <span className="text-[#282E29] text-sm">Uthma</span>
+                        {svgIcons.rightArrowIcon}
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            {/* Appearance Settings Section */}
-            <div className="px-6 pb-6">
-              <div className="flex items-center gap-4 cursor-pointer py-[10px] rounded-xl">
-                <div className="ml-2">
-                  <Image
-                    src="/icons/appearance-settings-icon.svg"
-                    alt="Appearance Settings"
-                    width={34}
-                    height={34}
-                  />
+              {/* View Settings Section */}
+              <div className="px-6 pb-6">
+                <div className="flex items-center gap-4 cursor-pointer py-[10px] rounded-xl">
+                  <div className="ml-2">
+                    <Image
+                      src="/icons/view-settings-icon.svg"
+                      alt="View Settings"
+                      width={34}
+                      height={34}
+                    />
+                  </div>
+                  <h3 className="text-[#282E29] text-sm font-medium flex-1">
+                    View Settings
+                  </h3>
+                  <div className="mr-2">
+                    <Image
+                      src="/icons/arrow-down.svg"
+                      alt="Arrow"
+                      width={14}
+                      height={14}
+                    />
+                  </div>
                 </div>
-                <h3 className="text-[#282E29] text-sm font-medium flex-1">
-                  Appearance Settings
-                </h3>
-                <div className="mr-2">
-                  <Image
-                    src="/icons/arrow-down.svg"
-                    alt="Arrow"
-                    width={14}
-                    height={14}
-                  />
+              </div>
+
+              {/* Appearance Settings Section */}
+              <div className="px-6 pb-6">
+                <div className="flex items-center gap-4 cursor-pointer py-[10px] rounded-xl">
+                  <div className="ml-2">
+                    <Image
+                      src="/icons/appearance-settings-icon.svg"
+                      alt="Appearance Settings"
+                      width={34}
+                      height={34}
+                    />
+                  </div>
+                  <h3 className="text-[#282E29] text-sm font-medium flex-1">
+                    Appearance Settings
+                  </h3>
+                  <div className="mr-2">
+                    <Image
+                      src="/icons/arrow-down.svg"
+                      alt="Arrow"
+                      width={14}
+                      height={14}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
